1c1d0464fef75c0fda43d394ce5f018b79c1f46a
Miles Peele <milesbpeele@gmail.com>
2023-10-31 16:10:58 -0400

    Add docker config

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,3 +1,5 @@
+import io.ktor.plugin.features.DockerImageRegistry
+
 plugins {
     kotlin("jvm")
     alias(libs.plugins.ktor)
@@ -8,8 +10,10 @@ plugins {
 group = "metal.diary"
 version = "0.0.1"
 
+val applicationClassName = "io.ktor.server.netty.EngineMain"
+
 application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+    mainClass.set(applicationClassName)
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
@@ -17,6 +21,28 @@ application {
 
 kotlin {
     jvmToolchain(libs.versions.jvmVersion.get().toInt())
+}
+
+ktor {
+    docker {
+        localImageName.set("diary-server-image")
+        imageTag.set("$version")
+        jreVersion.set(JavaVersion.toVersion(libs.versions.jvmVersion.get()))
+
+        externalRegistry.set(
+            DockerImageRegistry.dockerHub(
+                appName = provider { "diary-app" },
+                username = providers.environmentVariable("DOCKER_HUB_USERNAME"),
+                password = providers.environmentVariable("DOCKER_HUB_PASSWORD")
+            )
+        )
+    }
+}
+
+jib {
+    container {
+        mainClass = applicationClassName
+    }
 }
 
 dependencies {
