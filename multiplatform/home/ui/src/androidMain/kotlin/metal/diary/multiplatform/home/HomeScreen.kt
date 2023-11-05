b0d370c955d5752bc519f3c43280ff244e0e139b
Miles Peele <milesbpeele@gmail.com>
2023-11-05 14:02:11 -0500

    Fill weight in home bottombar

--- multiplatform/home/ui/src/androidMain/kotlin/metal/diary/multiplatform/home/HomeScreen.kt
+++ multiplatform/home/ui/src/androidMain/kotlin/metal/diary/multiplatform/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package metal.diary.multiplatform.home
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
@@ -8,19 +9,24 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeScreen() {
     Scaffold(
         bottomBar = {
-            BottomAppBar {
+            BottomAppBar(
+                modifier = Modifier.fillMaxWidth()
+            ) {
                 IconButton(
+                    modifier = Modifier.weight(1f),
                     onClick = { /* Handle navigation icon click */ }
                 ) {
                     Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
                 }
 
                 IconButton(
+                    modifier = Modifier.weight(1f),
                     onClick = { /* Handle navigation icon click */ }
                 ) {
                     Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
