ea64d04c535202293234d1bcdf0ed7af865baa19
Miles Peele <milesbpeele@gmail.com>
2023-11-07 09:00:45 -0500

    Scrollable ugly list

--- multiplatform/home/ui/src/androidMain/kotlin/metal/diary/multiplatform/home/HomeScreen.kt
+++ multiplatform/home/ui/src/androidMain/kotlin/metal/diary/multiplatform/home/HomeScreen.kt
@@ -1,7 +1,10 @@
 package metal.diary.multiplatform.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -10,6 +13,7 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -19,6 +23,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
 import metal.diary.addentry.nav.ADD_ENTRY_SCREEN_ROUTE
@@ -33,48 +38,49 @@ fun HomeScreen(navController: NavController, viewModel: HomeViewModel = koinView
         viewModel.getEntries()
     }
 
-    Column {
-        LazyColumn {
-            items(uiState.value.entries) { item: DiaryEntry ->
-                Column {
-                    Text(text = item.title)
-                    Text(text = item.body)
+    Scaffold(
+        content = {padding ->
+            padding.calculateBottomPadding()
+            
+            LazyColumn(contentPadding = PaddingValues(horizontal = 4.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)) {
+                items(uiState.value.entries) { item: DiaryEntry ->
+                    Column {
+                        Card(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+                            Text(text = item.title, modifier = Modifier.padding(4.dp))
+                            Text(text = item.body, modifier = Modifier.padding(4.dp))
+                        }
+                    }
+                }
+            }
+        },
+        bottomBar = {
+            BottomAppBar(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                IconButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = { /* Handle navigation icon click */ }
+                ) {
+                    Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
+                }
+
+                IconButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = { /* Handle navigation icon click */ }
+                ) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+                }
+
+                IconButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {
+                        navController.navigate(ADD_ENTRY_SCREEN_ROUTE)
+                    }
+                ) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
                 }
             }
         }
-        
-        Scaffold(
-            bottomBar = {
-                BottomAppBar(
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    IconButton(
-                        modifier = Modifier.weight(1f),
-                        onClick = { /* Handle navigation icon click */ }
-                    ) {
-                        Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
-                    }
-
-                    IconButton(
-                        modifier = Modifier.weight(1f),
-                        onClick = { /* Handle navigation icon click */ }
-                    ) {
-                        Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
-                    }
-
-                    IconButton(
-                        modifier = Modifier.weight(1f),
-                        onClick = {
-                            navController.navigate(ADD_ENTRY_SCREEN_ROUTE)
-                        }
-                    ) {
-                        Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
-                    }
-                }
-            },
-            content = { padding ->
-                padding.calculateBottomPadding()
-            }
-        )
-    }
+    )
 }
